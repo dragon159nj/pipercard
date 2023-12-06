233b8f42bdf36a254d8f261927d0988dacc801d9
Jacobsen_Nathaniel <jacon442@hsd.k12.or.us>
2023-12-06 15:30:36 -0800

    Added a ton of stuff

--- pypercard/main.py
+++ pypercard/main.py
@@ -18,24 +18,45 @@ def auto_func(app, card):
 
 
 # The templates for these cards can be found in pypercard.html.
-cards = [
-    Card("card1", auto_advance=5, transition="card2"),
-    Card("card2", auto_advance=5, transition=auto_func),
-    Card("card3", auto_advance=5, transition="card4"),
-    Card("card4", auto_advance=5, transition="card5"),
-    Card("card5", auto_advance=5, transition="card1")
-]
+# cards = [
+#     Card("card1"),
+#     Card("card2", ),
+#     Card("card3", 
+#     Card("card4", transition="card5"),
+#     Card("card5", transition="card6"),
+#     Card("card6", transition="card7"),
+#     Card("card7", transition="card8"),
+#     Card("card8", transition="card9"),
+#     Card("card9", transition="card10"),
+#     Card("card10", transition="card11")
+# ]
 
 
 # Create the app while ensuring the counter is reset.
 carousel_app = App(
-    name="PyperCard carousel", datastore={"counter": 0}, cards=cards
+    name="PyperCard carousel", datastore={"counter": 0}
 )
 
 
-@carousel_app.transition("card2", "click", "reset")
+@carousel_app.transition("morning", "click", "food")
 def reset(app, card):
-    return "card1"
+    return "boring_meal"
 
+@carousel_app.transition("morning", "click", "full-course")
+def reset(app, card):
+    return "big_meal"
 
-carousel_app.start("card1")
+@carousel_app.transition("big_meal", "click", "sleep")
+def next_card(app, card):
+    return "stay_home"
+
+@carousel_app.transition("boring_meal", "click", "stay")
+def next_card(app, card):
+    return "stay_home"
+
+@carousel_app.transition("stay_home", "click", "reset")
+def next_card(app, card):
+    return "morning"
+
+carousel_app.start("morning")
+
