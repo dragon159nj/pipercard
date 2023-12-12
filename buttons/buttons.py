140aaa2d8b8587f66d72967c916d29d0cb486372
dragon_159_nj <NaeNaejacobsen@gmail.com>
2023-12-12 15:09:37 -0800

    added buttons

--- buttons/buttons.py
+++ buttons/buttons.py
@@ -1,24 +1,62 @@
-""" A set of python functions for the index page.
-
-A set of functions to manipulate the DOM and add some interactivity
-to the home page."""
-
-# imports
-from pyodide.ffi import create_proxy
-from js import document
-
-
-# do something
-@create_proxy
-def do_something(*args):
-    """does something (up to the developer playing with this)"""
-    # TODO: come up with some python code to manipulate the DOM using msg
-    # for loop? something else?
-    # a sample has been provided to get you started
-    msg = "<p><strong>Hey</strong>! Just checking</p>"
-    document.getElementById("display-canvas").innerHTML = msg
+"""
+This simple app demonstrates how cards can automatically advance to another
+card after a certain amount of time. The auto_advance can either be a string
+containing the name of the next card, or a function to call that returns the
+name of the next card.
+"""
+from pypercard import App, Card
 
 
-document.getElementById("load_btn").addEventListener("click", do_something)
+def auto_func(app, card):
+    """
+    Called while transitioning from card 2, to card 3.
+    """
+    count = app.datastore.setdefault("counter", 0)
+    count += 1
+    app.datastore["counter"] = count
+    return "card3"
 
-# Call function
+
+# The templates for these cards can be found in pypercard.html.
+# cards = [
+#     Card("card1"),
+#     Card("card2", ),
+#     Card("card3", 
+#     Card("card4", transition="card5"),
+#     Card("card5", transition="card6"),
+#     Card("card6", transition="card7"),
+#     Card("card7", transition="card8"),
+#     Card("card8", transition="card9"),
+#     Card("card9", transition="card10"),
+#     Card("card10", transition="card11")
+# ]
+
+
+# Create the app while ensuring the counter is reset.
+carousel_app = App(
+    name="PyperCard carousel", datastore={"counter": 0}
+)
+
+
+@carousel_app.transition("morning", "click", "food")
+def reset(app, card):
+    return "boring_meal"
+
+@carousel_app.transition("morning", "click", "full-course")
+def reset(app, card):
+    return "big_meal"
+
+@carousel_app.transition("big_meal", "click", "sleep")
+def next_card(app, card):
+    return "stay_home"
+
+@carousel_app.transition("boring_meal", "click", "stay")
+def next_card(app, card):
+    return "stay_home"
+
+@carousel_app.transition("stay_home", "click", "reset")
+def next_card(app, card):
+    return "morning"
+
+carousel_app.start("morning")
+
